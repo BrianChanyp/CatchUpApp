c05e6061a3810fa08c074079112076116198f0a1
BrianChanyp <brianchanyp@gmail.com>
2022-09-23 19:07:30 +1200

    Theme applied

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -4,19 +4,19 @@ import { StatusBar, StyleSheet, SafeAreaView, Text, View } from "react-native";
 import styled from "styled-components/native";
 
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
+
 const SafeArea = styled(SafeAreaView)`
 	flex: 1;
 	margin-top: ${StatusBar.currentHeight}px;
 `;
 
 const SearchContainer = styled.View`
-	padding: 16px;
+	padding: ${(props) => props.theme.space[3]};
 `;
 
 const RestaurantListContainer = styled.View`
 	flex: 1;
-	padding: 16px;
-	background-color: blue;
+	padding: ${(props) => props.theme.space[3]};
 `;
 
 export const RestaurantsScreen = () => (
